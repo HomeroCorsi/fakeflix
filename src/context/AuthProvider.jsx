4d1a15821fe164f5276c3ac5f51ad6b8fdcf93de
FedericoSpina <FedericoSpina@hotmail.com>
2022-12-01 16:26:08 -0300

    NavBar LogOut
    LogOut

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,20 +2,23 @@ import { AuthContext } from "./AuthContext";
 import { initialState, authReducer } from "../reducer/authReducer";
 import { useReducer } from "react";
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
+  // console.log(state)
 
-    const [state, dispatch] = useReducer(authReducer, initialState)
-        
-    // console.log(state)
-return(
-<AuthContext.Provider value={{user: state.user, 
-                                token: state.token,
-                                isAuth: state.isAuth,
-                                dispatch,
-                                }}>{children}
-</AuthContext.Provider>
-)
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        token: state.token,
+        isAuth: state.isAuth,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
